2da2b0f8e3e7e0f4bb412bdd7e2bc99f755bf1a9
Corpus <jardel_corpus@hotmail.com>
2022-11-10 17:19:16 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/MaestrosController.cs
@@ -0,0 +1,87 @@
+﻿using Back_End_BD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Back_End_BD.Controllers
+{
+    public class MaestrosController : Controller
+    {
+        // GET: Maestros
+        public ActionResult Index()
+        {
+            using (AlumnoDbContext dbMaestros = new AlumnoDbContext())
+            {
+                //select * from alumnos;
+
+                return View(dbMaestros.Maestros.ToList());
+            }
+        }
+        public ActionResult Details(int? id)
+        {
+            using (AlumnoDbContext dbMaestros = new AlumnoDbContext())
+            {
+                Maestro maestro = dbMaestros.Maestros.Find(id);
+                if (maestro == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(maestro);
+            }
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Maestro prof)
+        {
+            using (AlumnoDbContext dbMaestros = new AlumnoDbContext())
+            {
+                dbMaestros.Maestros.Add(prof);
+                dbMaestros.SaveChanges();
+
+            }
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int? id)
+        {
+            using (AlumnoDbContext dbMaestros = new AlumnoDbContext())
+            {
+                return View(dbMaestros.Maestros.Where(x => x.Matrícula == id).FirstOrDefault());
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(Maestro prof)
+        {
+            using (AlumnoDbContext dbMaestros = new AlumnoDbContext())
+            {
+                dbMaestros.Entry(prof).State = EntityState.Modified;
+                dbMaestros.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int? id)
+        {
+            using (AlumnoDbContext dbMaestros = new AlumnoDbContext())
+            {
+                return View(dbMaestros.Maestros.Where(x => x.Matrícula == id).FirstOrDefault());
+            }
+        }
+        [HttpPost]
+        public ActionResult Delete(Maestro prof, int? id)
+        {
+            using (AlumnoDbContext dbMaestros = new AlumnoDbContext())
+            {
+                Maestro mae = dbMaestros.Maestros.Where(x => x.Matrícula == id).FirstOrDefault();
+                dbMaestros.Maestros.Remove(mae);
+                dbMaestros.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Maestro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Back_End_BD.Models
+{
+    public class Maestro
+    {
+        [Key]
+        public int Matrícula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Correo { get; set; }
+        public int Edad { get; set; }
+    }
+}
